0015364c3c38a018626a308bf3cfda4538017688
Таня <@DESKTOP-95N3LMH>
2021-09-29 23:49:30 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using ConsoleApp1.Tasks;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1.Solution();
+        }
+    }
+}
---
+++ ConsoleApp1/Tasks/Task1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Tasks
+{
+    //Задача вычисления факториала
+    static class Task1
+    {
+        private static int Factorial(int number)
+        {
+            if (number != 1)
+                return Factorial(number - 1) * number;
+            else
+                return 1;
+        }
+        public static void Solution()
+        {
+            Console.WriteLine(Factorial(5).ToString());
+        }
+    }
+}
